9dd574bc937f5b450a168c7241981ee050af8378
Jack Sleight <hi@jacksleight.com>
2014-06-06 17:08:13 +0100

    Fix type case sensitive names.

--- lib/Doctrine.php
+++ lib/Doctrine.php
@@ -9,17 +9,17 @@ namespace Coast\Doctrine;
 function register_dbal_types()
 {
     $types = [
-        'coast_json'    => 'Coast\Doctrine\DBAL\Types\JSONType',
-        'coast_url'        => 'Coast\Doctrine\DBAL\Types\URLType',
+        'coast_json' => 'Coast\Doctrine\DBAL\Types\JsonType',
+        'coast_url'  => 'Coast\Doctrine\DBAL\Types\UrlType',
     ];
     foreach ($types as $name => $class) {
         \Doctrine\DBAL\Types\Type::addType($name, $class);
     }
     if (class_exists('Carbon\Carbon')) {
         $types = [
-            'date'        => 'Coast\Doctrine\DBAL\Types\CarbonDateType',
-            'time'        => 'Coast\Doctrine\DBAL\Types\CarbonTimeType',
-            'datetime'    => 'Coast\Doctrine\DBAL\Types\CarbonDateTimeType',
+            'date'     => 'Coast\Doctrine\DBAL\Types\CarbonDateType',
+            'time'     => 'Coast\Doctrine\DBAL\Types\CarbonTimeType',
+            'datetime' => 'Coast\Doctrine\DBAL\Types\CarbonDateTimeType',
         ];
         foreach ($types as $name => $class) {
             \Doctrine\DBAL\Types\Type::overrideType($name, $class);
